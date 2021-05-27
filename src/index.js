1e014ac01beb3e3d5d237c52ed5024f4aea51e85
ABSphreak <absphreak@outlook.com>
2021-05-27 19:41:35 +0530

    ➕ Add dependencies

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { fastify } from 'fastify';
 import fastifyStatic from 'fastify-static';
+import fastifyCookie from 'fastify-cookie';
 import { connectDb } from './db.js';
 import { registerUser } from './accounts/register.js';
 import { authorizeUser } from './accounts/authorize.js';
+import { logUserIn } from './accounts/logUserIn.js';
 
 // To make __dirname available
 const __filename = fileURLToPath(import.meta.url);
@@ -20,6 +22,10 @@ async function startApp() {
 		// Serve static index.html
 		app.register(fastifyStatic, {
 			root: path.join(__dirname, 'public'),
+		});
+
+		app.register(fastifyCookie, {
+			secret: process.env.COOKIE_SIGNATURE,
 		});
 
 		app.post('/api/register', async (req, res) => {
@@ -37,7 +43,10 @@ async function startApp() {
 
 		app.post('/api/authorize', async (req, res) => {
 			try {
-				await authorizeUser(req.body.email, req.body.password);
+				const { isAuthorized, userId } = await authorizeUser(req.body.email, req.body.password);
+				if (isAuthorized) {
+					await logUserIn(userId, req, res);
+				}
 				res.send({
 					data: 'hello',
 				});
@@ -46,11 +55,13 @@ async function startApp() {
 			}
 		});
 
-		// app.get('/', {}, (req, res) => {
-		// 	res.send({
-		// 		data: 'Hello world!',
-		// 	});
-		// });
+		app.get('/test', {}, (req, res) => {
+			console.log(req.cookies);
+			res.send({
+				data: 'Hello world!',
+			});
+		});
+
 		app.listen(PORT);
 		console.log('🚀 Server listening on PORT:', PORT);
 	} catch (e) {
