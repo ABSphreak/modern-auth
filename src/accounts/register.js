657cedbedf1c24f55e50a3e522fdcf2cb5607426
ABSphreak <absphreak@outlook.com>
2021-05-27 18:59:08 +0530

    ➕ Complete Register function

---
+++ src/accounts/register.js
@@ -0,0 +1,30 @@
+import bcrypt from 'bcryptjs';
+
+const { genSalt, hash } = bcrypt;
+
+export async function registerUser(email, password) {
+	try {
+		// Dynamic Import
+		const { user } = await import('../models/user.js');
+
+		// Generate salt
+		const salt = await genSalt(10);
+
+		// Hash with salt
+		const hashedPassword = await hash(password, salt);
+
+		// Store in DB
+		const result = await user.insertOne({
+			email: {
+				address: email,
+				verified: false,
+			},
+			password: hashedPassword,
+		});
+
+		// Return user from DB
+		return result.insertedId;
+	} catch (e) {
+		console.error(e);
+	}
+}
