9a20ad77baad721a016d0efe3f30c6c9c1bb3582
ABSphreak <absphreak@outlook.com>
2021-05-28 20:38:46 +0530

    ➕ Add indexes

--- src/models/session.js
+++ src/models/session.js
@@ -1,3 +1,5 @@
 import { client } from '../db.js';
 
 export const session = client.db('modern-auth').collection('session');
+
+session.createIndex({ sessionToken: 1 });
--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,5 @@
 import { client } from '../db.js';
 
 export const user = client.db('modern-auth').collection('user');
+
+user.createIndex({ 'email.address': 1 });
