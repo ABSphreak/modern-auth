55178747448b3095535b7134bf57e73b84efbdd3
ABSphreak <absphreak@outlook.com>
2021-05-28 20:38:14 +0530

    🔧 Fix logout system

---
+++ src/accounts/logUserOut.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+const JWTSignature = process.env.JWT_SIGNATURE;
+
+export async function logUserOut(req, res) {
+	try {
+		const { session } = await import('../models/session.js');
+		// Get refreshToken
+		// Decode Refresh Token
+		if (req?.cookies?.refreshToken) {
+			const { refreshToken } = req.cookies;
+
+			// Decode sessionToken from refreshToken
+			const { sessionToken } = jwt.verify(refreshToken, JWTSignature);
+
+			// Delete db record for session
+			await session.deleteOne({ sessionToken });
+		}
+		// Remove cookies
+		res.clearCookie('accessToken').clearCookie('refreshToken');
+	} catch (e) {
+		console.error(e);
+	}
+}
