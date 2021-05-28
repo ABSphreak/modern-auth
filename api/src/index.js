50d7949c2208582114aadd18bc68ca761c807624
ABSphreak <absphreak@outlook.com>
2021-05-29 01:16:08 +0530

    ➕ Add routes and refactor

--- api/src/index.js
+++ api/src/index.js
@@ -7,9 +7,9 @@ import { registerUser } from './accounts/register.js';
 import { authorizeUser } from './accounts/authorize.js';
 import { logUserIn } from './accounts/logUserIn.js';
 import { logUserOut } from './accounts/logUserOut.js';
-import { getUserFromCookies } from './accounts/user.js';
+import { changePassword, getUserFromCookies } from './accounts/user.js';
 import { mailInit, sendEmail } from './mail/index.js';
-import { createVerifyEmailLink } from './accounts/verify.js';
+import { createVerifyEmailLink, validateVerifyEmail } from './accounts/verify.js';
 
 const PORT = process.env.PORT || 5000;
 
@@ -28,7 +28,7 @@ async function startApp() {
 			credentials: true,
 		});
 
-		app.post('/api/register', async (req, res) => {
+		app.post('/api/register', {}, async (req, res) => {
 			try {
 				const userId = await registerUser(req.body.email, req.body.password);
 				if (userId) {
@@ -58,7 +58,7 @@ async function startApp() {
 			}
 		});
 
-		app.post('/api/authorize', async (req, res) => {
+		app.post('/api/authorize', {}, async (req, res) => {
 			try {
 				const { isAuthorized, userId } = await authorizeUser(req.body.email, req.body.password);
 				if (isAuthorized) {
@@ -108,18 +108,69 @@ async function startApp() {
 						},
 					})
 					.code(500);
-				throw new Error(e);
 			}
 		});
 
-		app.post('/api/logout', async (req, res) => {
+		app.post('/api/logout', {}, async (req, res) => {
 			try {
 				await logUserOut(req, res);
 				res.send({
-					data: 'User logged out',
+					data: {
+						status: 'SUCCESS',
+					},
 				});
 			} catch (e) {
-				console.error(e);
+				res
+					.send({
+						data: {
+							status: 'FAILED',
+						},
+					})
+					.code(500);
+			}
+		});
+
+		app.post('/api/verify', {}, async (req, res) => {
+			try {
+				const { token, email } = req.body;
+				const isValid = await validateVerifyEmail(token, email);
+				if (isValid) {
+					res.code(200).send();
+				}
+				res.code(401).send({
+					data: 'SUCCESS',
+				});
+			} catch (e) {
+				res
+					.send({
+						data: {
+							status: 'FAILED',
+						},
+					})
+					.code(401);
+			}
+		});
+
+		app.post('/api/change-password', {}, async (req, res) => {
+			try {
+				const { oldPassword, newPassword } = req.body;
+				const user = await getUserFromCookies(req, res);
+				const { isAuthorized, userId } = await authorizeUser(user.email.address, oldPassword);
+				if (isAuthorized) {
+					// Update in db
+					await changePassword(userId, newPassword);
+					res.code(200).send('cool');
+				} else {
+					res.code(401).send();
+				}
+			} catch (e) {
+				res
+					.send({
+						data: {
+							status: 'FAILED',
+						},
+					})
+					.code(401);
 			}
 		});
 
